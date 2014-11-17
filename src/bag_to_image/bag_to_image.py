6b9f86395be026cf7e4660167a3cb77f7ae79806
Christian Forster <ch.forster@gmail.com>
2014-11-17 15:08:58 -0500

    add unique id to every line

--- src/bag_to_image/bag_to_image.py
+++ src/bag_to_image/bag_to_image.py
@@ -3,24 +3,31 @@
 import rosbag
 import argparse
 import cv
+import numpy as np
 from cv_bridge import CvBridge, CvBridgeError
 
 def extract(bagfile, pose_topic, out_filename):
     n = 0
     f = open(out_filename, 'w')
-    f.write('# timestamp image_name')
+    f.write('# id timestamp image_name\n')
     cv_bridge = CvBridge()
+    extract_every_nth_image = 1
+    #max_imgs = 400
     with rosbag.Bag(bagfile, 'r') as bag:
         for (topic, msg, ts) in bag.read_messages(topics=str(pose_topic)):
-            try:
-                img = cv_bridge.imgmsg_to_cv(msg, "bgr8")
-            except CvBridgeError, e:
-                print e
-            ts = msg.header.stamp.to_sec()
-            image_name = 'image_'+str(n)+'.png'
-            f.write('%.12f %s \n' % (ts, image_name))
-            cv.SaveImage(image_name, img)
+            if np.mod(n, extract_every_nth_image) == 0:
+                try:
+                    img = cv_bridge.imgmsg_to_cv(msg, "bgr8")
+                except CvBridgeError, e:
+                    print e
+                    
+                ts = msg.header.stamp.to_sec()
+                image_name = 'image_'+str(n)+'.png'
+                f.write('%d %.12f img/%s \n' % (n, ts, image_name))
+                cv.SaveImage(image_name, img)
             n += 1
+            #if n > max_imgs:
+            #    break
             
     print('wrote ' + str(n) + ' images messages to the file: ' + out_filename)
           
